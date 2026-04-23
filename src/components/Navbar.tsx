1c5e7ac4a4056e0e3d130ca9e40401c2e130675f
ikkxeer <samuiker2427@gmail.com>
2026-04-23 12:34:59 +0200

    Modificaciones nuevo menu de navegacion (Hamburgues) y modificaciones Responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,28 @@
+import { useState } from 'react';
 import './Navbar.css'
 
 export default function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="logo">
                 <h2>Iker Aramburu Muñoz</h2>
             </div>
 
-            <ul className="nav-links">
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#proyectos">Proyectos</a></li>
-                <li><a href="#">Contacto</a></li>
+            {/* Boton menu hamburguesa */}
+            <div className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={() => setIsOpen(!isOpen)}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
 
+            <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                <li><a href="#" onClick={() => setIsOpen(false)}>Inicio</a></li>
+                <li><a href="#proyectos" onClick={() => setIsOpen(false)}>Proyectos</a></li>
+                <li><a href="#tecnologias" onClick={() => setIsOpen(false)}>Tecnologías</a></li>
+                <li><a href="#" onClick={() => setIsOpen(false)}>Contacto</a></li>
             </ul>
         </nav>
-
     )
 }
