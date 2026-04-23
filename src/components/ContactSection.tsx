2bafd6366acb8d65e7ae865770187d56b47a8d2b
ikkxeer <samuiker2427@gmail.com>
2026-04-23 13:19:17 +0200

    Base de la seccion de Contacto

---
+++ src/components/ContactSection.tsx
@@ -0,0 +1,11 @@
+import './ContactSection.css';
+
+export default function ContactSection {
+    return (
+        <section className="contact-section" id="contacto">
+            <h2 className="section-title">Contacto</h2>
+
+        </section>
+    )
+
+}
