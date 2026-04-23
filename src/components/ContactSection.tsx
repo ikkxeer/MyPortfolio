19a9e8b7da44c080b4cef45917990c94f5674a84
iker <samuiker2427@gmail.com>
2026-04-23 22:29:27 +0200

    Seccion de contacto y de videos

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -4,6 +4,40 @@ export default function ContactSection() {
     return (
         <section className="contact-section" id="contacto">
             <h2 className="section-title">Contacto</h2>
+            <p className='contact-description'>¿Tienes un proyecto en mente? ¡Escríbeme y hablemos!</p>
+            
+            <form action="https://formspree.io/f/ID_EN_TEORIA" method="POST" className="contact-form">
+                <div className="input-group">
+                        <input
+                            type="text"
+                            name="nombre"
+                            className="form-input"
+                            placeholder="Introduce tu nombre"
+                            required
+                        />
+                        <input
+                            type="email"
+                            name="email"
+                            className="form-input"
+                            placeholder="Introduce tu email"
+                            required
+                        />
+                        </div>
+
+                        <textarea
+                        name="mensaje"
+                        className="form-textarea"
+                        placeholder="¿En qué te puedo ayudar?"
+                        rows={5}
+                        required
+                        ></textarea>
+
+                        <button type="submit" className="submit-btn">
+                        Enviar Mensaje
+                        </button>
+
+            </form>
+
         </section>
     )
 }
--- src/components/VideosSection.tsx
+++ src/components/VideosSection.tsx
@@ -1,9 +1,40 @@
 import './VideosSection.css';
 
+const videos = [
+    {
+        id: 1,
+        title: 'Video 1',
+        ruta: '/video2.mp4'
+    },
+    {
+        id: 2,
+        title: 'Video 2',
+        ruta: '/video1.mp4'
+    }
+]
+
 export default function VideosSection() {
     return (
         <section className='videos-section' id='videos'>
             <h2 className='section-title'>Videos</h2>
+
+            <div className="video-container">
+                {videos.map((video) => (
+                    <div key={video.id} className="video">
+                        <iframe
+                            width="560"
+                            height="315"
+                            src={video.ruta}
+                            title={video.title}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            referrerPolicy="strict-origin-when-cross-origin"
+                            allowFullScreen
+                        ></iframe>
+                    </div>
+                ))}
+            </div>
+
         </section>
+
     )
 }
